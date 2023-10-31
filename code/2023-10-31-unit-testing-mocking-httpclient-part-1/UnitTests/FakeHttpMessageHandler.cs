5f3aa97352e6cb7ba308bd241cee0ef080125506
Alex Schouls <alex_schouls@live.com>
2023-10-31 15:28:38 +0100

    added unit tests for the typed http client call

---
+++ code/2023-10-31-unit-testing-mocking-httpclient-part-1/UnitTests/FakeHttpMessageHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace UnitTests;
+
+internal class FakeHttpMessageHandler : HttpMessageHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var jsonUserData = @"
+        [    
+            {
+                ""id"": 9,
+                ""name"": ""Glenna Reichert"",
+                ""username"": ""Delphine"",
+                ""email"": ""Chaim_McDermott@dana.io"",
+                ""phone"": ""(775)976-6794 x41206"",
+                ""website"": ""conrad.com""
+            },
+            {
+                ""id"": 10,
+                ""name"": ""Clementina DuBuque"",
+                ""username"": ""Moriah.Stanton"",
+                ""email"": ""Rey.Padberg@karina.biz"",
+                ""phone"": ""024-648-3804"",
+                ""website"": ""ambrose.net""
+            }
+        ]";
+
+        return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StringContent(jsonUserData)
+        });
+    }
+}
---
+++ code/2023-10-31-unit-testing-mocking-httpclient-part-1/UnitTests/UsersRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using API;
+using FluentAssertions;
+using Xunit;
+
+namespace UnitTests;
+
+public class UsersRepositoryTests
+{
+    [Fact]
+    public async Task GetUsersAsync_WithSuccessResponse_ShouldReturnUsers()
+    {
+        // Arrange
+        var expectedUsers = new List<User>
+        {
+            new("Glenna Reichert", "Chaim_McDermott@dana.io"),
+            new("Clementina DuBuque", "Rey.Padberg@karina.biz")
+        };
+
+        var httpMessageHandler = new FakeHttpMessageHandler();
+        var httpClient = new HttpClient(httpMessageHandler) { BaseAddress = new Uri("http://unit.testing.local") };
+        var usersRepository = new UsersRepository(httpClient);
+
+        // Act
+        var actualUsers = await usersRepository.GetUsersAsync();
+
+        // Assert
+        actualUsers.Should().BeEquivalentTo(expectedUsers);
+    }
+}
